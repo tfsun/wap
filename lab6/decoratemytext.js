e757fdcdb52834a031d5df4e5e9fa5368b6a44cb
tfsun <tsun@mum.eduu_macpro>
2015-08-03 23:51:02 -0500

    part of lab6

--- lab6/decoratemytext.js
+++ lab6/decoratemytext.js
@@ -1,7 +1,41 @@
 function pageLoad() {
   document.getElementById("biger").onclick = makebiger;
+  document.getElementById("bling").onchange = bling;
+  document.getElementById("Malkovitch").onclick = Malkovitch;
+}
+var timer = null;
+var size = 12;
+function makebiger() {
+  if (timer == null) {
+    timer = setInterval(biger, 500);
+  } else {
+    clearInterval(timer);
+    timer = null;
+  }
+}
+function biger()
+{
+    size += 2;
+    document.getElementById("texta").style.fontSize = size + "pt";
+}
+function bling() {
+  alert("bling!");
+  if(document.getElementById("bling").checked==true)
+  {
+      document.getElementById("texta").className="textChecked";
+      document.getElementById("body").className="bodyChecked";
+  }
+  else {
+      document.getElementById("texta").className="textNormal";
+      document.getElementById("body").className="";
+  }
 }
 
-function makebiger() {
-  alert("Hello, world!");
+function Malkovitch() {
+  var text = document.getElementById("texta").innerHTML;
+  if(text.length>5) {
+      document.getElementById("texta").innerHTML = "                    Malkovitch";
+  }
 }
+
+window.onload = pageLoad; 
