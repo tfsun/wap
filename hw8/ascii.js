d69a063f4f3a933c54505c44080bc5d726d0cc0e
tfsun <tsun@mum.eduu_macpro>
2015-08-04 17:55:38 -0500

    part of hw8

---
+++ hw8/ascii.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    document.getElementById("start").onclick = start;
+    //document.getElementById("stop").onclick = stop;
+    //document.getElementById("animation").onchange = animation;
+};
+function start() {
+    alert("hhh");
+}
