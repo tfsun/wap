8e3a823adb96daad307b9c96b6eb6011821a5b20
tfsun <tsun@mum.eduu_macpro>
2015-08-07 00:44:02 -0500

    lab7

--- lab7/maze.js
+++ lab7/maze.js
@@ -1,21 +1,43 @@
+"use strict";
 $(document).ready(function(){
+    var beStart = false;
+    var win  = true;
     $(".boundary").mouseover(function(){
-        $(".boundary").css("background-color","red");
-        $("#status").text("Sorry, you lost.:[");
-        //alert("Sorry, you lost.:[");
+        checkHit();
+    }); 
+        $(".example").mouseover(function(){
+        checkHit();
     }); 
     $("#end").mouseover(function(){
-        $("#status").text("You win!:]");
-        //alert("You win!:]");
+        if(beStart === true && win === true){
+            $("#status").text("You win!:]");        
+        }
+        beStart = false;
+        setTimeout(function(){
+                $("#status").text('Click the "S" to begin.');  
+            },1000);
     });
     $("#start").click(function(){
         $(".boundary").css("background-color","#eeeeee");
+        $("#status").text('now start.');  
+        beStart = true;
+        win = true;
     });
-//    $("#body").mouseover(function) () { 
-//    });
-//    $(".boundary").mouseout(function(){
-//        $(".boundary").css("background-color","#eeeeee");
-//    });  
+    function checkHit(){
+        if(beStart === true) {
+            $(".boundary").css("background-color","red");
+            $("#status").text("Sorry, you lost.:[");
+            win = false;
+            beStart = false;
+            setTimeout(function(){
+                $("#status").text('Click the "S" to begin.');  
+            },1000);
+        }
+    }
+    $("#maze").after('<div id="hidden" style="font-size: 5px; width:100%;">.</div>');
+    $("#maze").siblings().mouseover(function(){
+        checkHit();
+    }); 
 });
 
 
