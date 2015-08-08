296a06a97ea3a3f912f78d1612c9cff4cf99ebda
tfsun <tsun@mum.edu>
2015-08-08 14:33:40 -0500

    jqueryquiz1

--- mytest/index.js
+++ mytest/index.js
@@ -1,31 +1,4 @@
-$(document).ready(function(){
-    //$("p").hide();
-//    $("#button").click(function(){
-//        //$("span.Italian").toggle();
-//        $("p#mytext").show();
-//    });
-//    $("div").click(function() {
-//        alert("You clicked me."); 
-//    });
-//    $(".guess_box").click( function() {
-//        var discount = Math.floor((Math.random()*5) + 5);
-//        var discount_msg = "<p>Your Discount is "+ discount +"%</p>";
-//        alert(discount);
-//    });
-    $(".guess_box").click( function() {
-        $(".guess_box p").remove();
-        var discount = Math.floor((Math.random()*5) + 5);
-        var discount_msg = "<p>Your Discount is "+discount+"%</p>";
-        alert(discount_msg);
-        $(this).append(discount_msg);
-    });
-    function booyah1(){
-    alert("BOOYAH!");
-}
-function booyah2(){
-    return setTimeout(function(){alert("BOOYAH!");}, 2000);
-}
-//setTimeout(booyah1, 2000);
-setTimeout(booyah2(), 2000);
-});
+var f = function() {alert("Yea, it's Saturday!");};
+f["temperature"] = "72 degrees";
+f["displayTemperature"] = function(){ alert(f["temperature"]);};
 
